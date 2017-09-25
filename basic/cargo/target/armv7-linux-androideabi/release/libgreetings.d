662d665feb79f59ef97c15fb89168f417ef4c58e
Emily Toop <etoop@mozilla.com>
2017-09-25 13:43:24 +0100

    update libs

--- basic/cargo/target/armv7-linux-androideabi/release/libgreetings.d
+++ basic/cargo/target/armv7-linux-androideabi/release/libgreetings.d
@@ -1,1 +1,1 @@
-/Users/emilytoop/Development/greetings/cargo/target/armv7-linux-androideabi/release/libgreetings.so: /Users/emilytoop/Development/greetings/cargo/src/lib.rs
+/Users/emilytoop/Development/greetings/basic/cargo/target/armv7-linux-androideabi/release/libgreetings.so: /Users/emilytoop/Development/greetings/basic/cargo/src/lib.rs
