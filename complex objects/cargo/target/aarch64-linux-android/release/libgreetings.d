b439c8f9cd0763cd5479d9e88204e812e20c6347
Emily Toop <etoop@mozilla.com>
2017-11-13 14:30:07 +0000

    Remove target from git

--- complex objects/cargo/target/aarch64-linux-android/release/libgreetings.d
+++
@@ -1,1 +0,0 @@
-/Users/emilytoop/Development/greetings/basic/cargo/target/aarch64-linux-android/release/libgreetings.so: /Users/emilytoop/Development/greetings/basic/cargo/src/lib.rs
--- complex objects/cargo/target/i686-linux-android/release/libgreetings.d
+++
@@ -1,1 +0,0 @@
-/Users/emilytoop/Development/greetings/basic/cargo/target/i686-linux-android/release/libgreetings.so: /Users/emilytoop/Development/greetings/basic/cargo/src/lib.rs
