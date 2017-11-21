be6ab0f8115eae49cad95246290b9619d9155f73
Emily Toop <etoop@mozilla.com>
2017-11-21 17:25:35 +0000

    Replace SQLite DB with Mentat DB in store crate

--- complex objects/cargo/store/src/lib.rs
+++ complex objects/cargo/store/src/lib.rs
@@ -8,9 +8,15 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
+extern crate mentat;
+extern crate edn;
+extern crate mentat_query;
+extern crate mentat_core;
+extern crate mentat_db;
 extern crate rusqlite;
 extern crate ffi_utils;
 
+use std::fmt;
 use std::os::raw::{
     c_char
 };
@@ -19,17 +25,20 @@ use std::sync::{
     Mutex
 };
 
-use rusqlite::{
-    Connection
-};
-
 use ffi_utils::strings::c_char_to_string;
+use mentat::{
+    new_connection,
+};
+use mentat::conn::Conn;
+use mentat_db::types::TxReport;
+use mentat::query::QueryResults;
 
-#[derive(Debug)]
+
 #[repr(C)]
 /// Store containing a SQLite connection
 pub struct Store {
-    conn: Mutex<Arc<Connection>>,
+    handle: rusqlite::Connection,
+    conn: Conn,
     uri: String,
 }
 
@@ -39,27 +48,47 @@ impl Drop for Store {
     }
 }
 
+impl fmt::Debug for Store {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Store at {:?}", self.uri)
+    }
+}
+
 impl Store {
-    pub fn new(uri: String) -> Self {
-        Store {
-            conn: Mutex::new(Arc::new(Connection::open(uri.clone()).unwrap())),
-            uri: uri.clone(),
-        }
+    pub fn new(uri: String) -> Result<Self, mentat::errors::Error> {
+        let mut h = try!(new_connection(&uri));
+        let c = try!(Conn::connect(&mut h));
+        Ok(Store {
+            handle: h,
+            conn:c,
+            uri: uri,
+        })
     }
 
-    pub fn write_connection(&self) -> Arc<Connection> {
-        self.conn.lock().unwrap().clone()
+    pub fn open(&mut self, uri: String) -> Result<(), mentat::errors::Error> {
+        self.handle = try!(new_connection(&uri));
+        self.conn = try!(Conn::connect(&mut self.handle));
+        self.uri = uri;
+        Ok(())
     }
 
-    pub fn read_connection(&self) -> Arc<Connection> {
-        Arc::new(Connection::open_with_flags(self.uri.clone(), rusqlite::SQLITE_OPEN_READ_ONLY).unwrap())
+    pub fn query(&self, query: String) -> Result<QueryResults, mentat::errors::Error> {
+        Ok(self.conn.q_once(&self.handle, &query, None)?)
+    }
+
+    pub fn transact(&mut self, transaction: String) -> Result<TxReport, mentat::errors::Error> {
+        Ok(self.conn.transact(&mut self.handle, &transaction)?)
+    }
+
+    pub fn fetch_schema(&self) -> edn::Value {
+        self.conn.current_schema().to_edn_value()
     }
 }
 
 #[no_mangle]
-pub extern "C" fn new_store(uri: *const c_char) -> *mut Arc<Store> {
+pub extern "C" fn new_store(uri: *const c_char) -> *mut Store {
     let uri = c_char_to_string(uri);
-    let store = Arc::new(Store::new(uri));
+    let store = Store::new(uri).expect("Expected Store");
     Box::into_raw(Box::new(store))
 }
 
