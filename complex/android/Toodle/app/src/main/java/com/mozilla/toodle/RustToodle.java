7327b06b98b5ec56ffcab21ebcf41993613d805d
Grisha Kruglov <gkruglov@mozilla.com>
2017-11-24 04:29:59 -0500

    Remove login manager stuff from java

--- complex/android/Toodle/app/src/main/java/com/mozilla/toodle/RustToodle.java
+++ complex/android/Toodle/app/src/main/java/com/mozilla/toodle/RustToodle.java
@@ -11,18 +11,15 @@ class RustToodle {
 
     private Long toodlePtr;
     private final String dbPath;
-    final LoginManager loginManager;
 
     private static RustToodle instance;
 
     private RustToodle(String dbPath) {
         this.dbPath = dbPath;
-        this.loginManager = new LoginManager(this);
     }
 
     void resume() {
         toodlePtr = newToodle(dbPath);
-        loginManager.refresh();
     }
 
     void pause() {
@@ -37,29 +34,23 @@ class RustToodle {
         return instance;
     }
 
-    static class LoginManager {
-        private Long loginManagerPtr;
-        private final RustToodle toodle;
-
-        LoginManager(RustToodle toodle) {
-            this.toodle = toodle;
-        }
-
-        private void refresh() {
-            loginManagerPtr = loginManager(toodle.toodlePtr);
-        }
-
-        Object validate(String username, String password) {
-            return validateLogin(loginManagerPtr, username, password);
-        }
-    }
+//    static class LoginManager {
+//        private Long loginManagerPtr;
+//        private final RustToodle toodle;
+//
+//        LoginManager(RustToodle toodle) {
+//            this.toodle = toodle;
+//        }
+//
+//        private void refresh() {
+//            loginManagerPtr = loginManager(toodle.toodlePtr);
+//        }
+//
+//        Object validate(String username, String password) {
+//            return validateLogin(loginManagerPtr, username, password);
+//        }
+//    }
 
     private static native long newToodle(final String dbPath);
     private static native void toodleDestroy(final long toodlePtr);
-
-    private static native long loginManager(final long toodlePtr);
-    private static native long validateLogin(final long toodlePtr, final String username, final String password);
-
-    private static native void toodleList(final long toodlePtr);
-    private static native void newCategory(final long toodlePtr, final String name);
 }
