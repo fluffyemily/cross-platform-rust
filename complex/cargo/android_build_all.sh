cd30870340788e106773c00bff57aa625709ad7c
Grisha Kruglov <gkruglov@mozilla.com>
2017-11-24 03:37:31 -0500

    debug builds

--- complex/cargo/android_build_all.sh
+++ complex/cargo/android_build_all.sh
@@ -1,3 +1,3 @@
-cargo build --target i686-linux-android --release
-cargo build --target armv7-linux-androideabi --release
-cargo build --target aarch64-linux-android --release
+cargo build --target i686-linux-android
+cargo build --target armv7-linux-androideabi
+cargo build --target aarch64-linux-android
