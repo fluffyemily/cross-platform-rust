57bc23405fa417eca5e4824d4c859e28451ff9e9
Grisha Kruglov <gkruglov@mozilla.com>
2017-11-24 04:33:26 -0500

    comment

--- complex/cargo/ffi-utils/src/android_ffi.rs
+++ complex/cargo/ffi-utils/src/android_ffi.rs
@@ -1,7 +1,9 @@
+// TODO just use https://github.com/tomaka/android-rs-glue somehow?
+
 use std::os::raw::c_char;
 use std::os::raw::c_int;
 
-//       android/log.h
+// Logging
 pub const ANDROID_LOG_DEBUG: i32 = 3;
 pub const ANDROID_LOG_INFO: i32 = 4;
 pub const ANDROID_LOG_WARN: i32 = 5;
